d2d84a59a0941f9efadfe967fc9937ce6c8a5bda
claudiob <claudiob@users.noreply.github.com>
2026-09-09 15:04:35 -0700

    Order what an account answers as the vocabulary does

--- lib/hcp/account.rb
+++ lib/hcp/account.rb
@@ -10,10 +10,10 @@ module Hcp
     # @return [Business] business the key belongs to, as the location it acts as.
     def business = Business.new node: @client.get('company')
 
-    # @return [Leads] leads of the location, to open and to move.
-    def leads = Leads.new client: @client
-
     # @return [Jobs] jobs of the location, walked a page at a time.
     def jobs = Jobs.new client: @client
+
+    # @return [Leads] leads of the location, to open and to move.
+    def leads = Leads.new client: @client
   end
 end
